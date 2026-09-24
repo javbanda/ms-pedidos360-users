38e0c206d8e3e42476621c4e5ddb14a56b9a5b00
javbanda <jav.banda@duocuc.cl>
2026-09-23 21:06:15 -0300

    Se crea el controlador de usuario

---
+++ src/main/java/com/pedidos360/users/controller/UsuarioController.java
@@ -0,0 +1,38 @@
+package com.pedidos360.users.controller;
+
+import com.pedidos360.users.dto.UsuarioDto;
+import com.pedidos360.users.entity.Usuario;
+import com.pedidos360.users.service.UsuarioService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@CrossOrigin(origins = "http://localhost:4200")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listarUsuarios() {
+        return ResponseEntity.ok(usuarioService.listarUsuarios());
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> crearUsuario(@RequestBody UsuarioDto usuarioDto) {
+
+        Usuario usuarioCreado = usuarioService.crearUsuario(usuarioDto);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(usuarioCreado);
+    }
+}
